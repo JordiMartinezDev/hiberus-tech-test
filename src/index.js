58702f45184098cb3b3de69d5cafbe642afad5da
JordiMartinezDev <jordiupc87@gmail.com>
2024-09-04 06:56:52 +0200

    index.js -> Missing exports added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const groupByInitial = require("./groupByInitial");
+const duplicateFilter = require("./duplicateFilter");
+const datesBetweenDates = require("./datesBetweenDates");
+const fetchAndSort = require("./fetchAndSort");
 
 module.exports = {
   groupByInitial,
+  duplicateFilter,
+  datesBetweenDates,
+  fetchAndSort,
 };
