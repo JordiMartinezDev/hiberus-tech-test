121f2cbe1f7f4c7baa810b1a7dd06321c1806dc4
JordiMartinezDev <jordiupc87@gmail.com>
2024-09-04 08:38:59 +0200

    Exercise 1 groupByInitial + tests

--- src/groupByInitial.js
+++ src/groupByInitial.js
@@ -1,1 +1,34 @@
-function groubByInitial(namesList) {}
+function groupByInitial(namesList) {
+  const groupedNames = {};
+
+  namesList.forEach((name) => {
+    //check if name is valid
+    if (!isValidName(name)) return;
+
+    // Normalizing names before any comparison as we should ignore the capitalization
+    const normalizedName = name.toUpperCase();
+    const initial = getFirstLetter(normalizedName);
+
+    if (!groupedNames[initial]) {
+      groupedNames[initial] = [];
+    }
+    groupedNames[initial].push(name);
+  });
+
+  return groupedNames;
+}
+
+function getFirstLetter(word) {
+  return word[0];
+}
+
+function isValidName(name) {
+  //Even if the exercise didn't require this, I wanted a maintainable way to check whether or not the names were valid that can be modified as needed
+
+  if (name == undefined || name == null || name == "" || name[0] == " ") {
+    return false;
+  }
+  return true;
+}
+
+module.exports = groupByInitial;
--- test/groupByInitial.test.js
+++ test/groupByInitial.test.js
@@ -1,1 +1,75 @@
+// Using index.js as central export
 const { groupByInitial } = require("../src");
+
+// ---- Test 1 - Basic ----
+
+test("groups names by initial correctly", () => {
+  const basicNameList = [
+    "Ana",
+    "Alberto",
+    "Beatriz",
+    "Bruno",
+    "Carlos",
+    "Clara",
+  ];
+
+  const expectedBasicNameList = {
+    A: ["Ana", "Alberto"],
+    B: ["Beatriz", "Bruno"],
+    C: ["Carlos", "Clara"],
+  };
+
+  expect(groupByInitial(basicNameList)).toEqual(expectedBasicNameList);
+});
+
+// ---- Test 2 - Capitalization ----
+
+test("handles nbames with multiple capitalization", () => {
+  const multipleCapitalization = ["Mario", "Miguel", "marta", "Monica"];
+
+  const expectedMultipleCapitalization = {
+    M: ["Mario", "Miguel", "marta", "Monica"],
+  };
+
+  expect(groupByInitial(multipleCapitalization)).toEqual(
+    expectedMultipleCapitalization
+  );
+});
+
+// ---- Test 3 - Duplicates ----
+
+test("handles duplicated names correctly", () => {
+  const duplicatedNames = ["Ana", "Alberto", "Ana", "Bruno", "Ana", "Clara"];
+
+  const expectedDuplicatedNames = {
+    A: ["Ana", "Alberto", "Ana", "Ana"],
+    B: ["Bruno"],
+    C: ["Clara"],
+  };
+  expect(groupByInitial(duplicatedNames)).toEqual(expectedDuplicatedNames);
+});
+
+// ---- Test 4 - Empty "" input ----
+
+test("avoids empty input", () => {
+  const emptyNames = ["Ana", "Jordi", ""];
+
+  const expectedEmptyNames = {
+    A: ["Ana"],
+    J: ["Jordi"],
+  };
+
+  expect(groupByInitial(emptyNames)).toEqual(expectedEmptyNames);
+});
+
+// ---- Test 5 - Space " " input ----
+
+test("avoids spaces and names starting with space", () => {
+  const spaceNames = ["Ana", " ", "Jordi", " Bob"];
+
+  const expectedSpaceNames = {
+    A: ["Ana"],
+    J: ["Jordi"],
+  };
+  expect(groupByInitial(spaceNames)).toEqual(expectedSpaceNames);
+});
