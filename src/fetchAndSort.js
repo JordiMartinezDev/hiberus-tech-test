805a7e86835383ff7837db0b30b4b21ddfd0991e
JordiMartinezDev <jordiupc87@gmail.com>
2024-09-04 16:43:08 +0200

    Updated daysBetweenDates tests + added fetchAndSort logic&tests

--- src/fetchAndSort.js
+++ src/fetchAndSort.js
@@ -11,7 +11,41 @@ async function fetchBreeds() {
 }
 
 function sortBreedsByPattern(breeds) {
-  // Working on code..
+  let hasSwapped = true;
+
+  while (hasSwapped) {
+    hasSwapped = false; // Assume no swaps will be made
+
+    for (let i = 0; i < breeds.length - 1; i++) {
+      let currentBreed = breeds[i];
+      let nextBreed = breeds[i + 1];
+
+      // Determine if these breeds need to be swapped
+      const shouldSwap = shouldSwapBreeds(currentBreed, nextBreed);
+
+      if (shouldSwap) {
+        // Swap the breeds
+        breeds[i] = nextBreed;
+        breeds[i + 1] = currentBreed;
+        hasSwapped = true; // Since a swap was made, we need to check again
+      }
+    }
+  }
+
   return breeds;
 }
+
+// Helper function to determine if two breeds should be swapped
+function shouldSwapBreeds(currentBreed, nextBreed) {
+  const currentPattern = currentBreed.pattern;
+  const nextPattern = nextBreed.pattern;
+
+  // Alphabetical order for non-empty patterns
+  if (currentPattern > nextPattern) {
+    return true;
+  }
+
+  return false; // No swap needed
+}
+
 module.exports = fetchAndSort;
--- test/daysBetweenDates.test.js
+++ test/daysBetweenDates.test.js
@@ -8,3 +8,21 @@ test("calculates correct day difference ( positive )", () => {
 
   expect(daysBetweenDates(date1, date2)).toEqual(daysExpected);
 });
+
+test("calculates correct day difference ( negative )", () => {
+  const date1 = "2024-01-24";
+  const date2 = "2024-01-14";
+
+  const daysExpected = -10;
+
+  expect(daysBetweenDates(date1, date2)).toEqual(daysExpected);
+});
+
+test("calculates correct day difference ( zero )", () => {
+  const date1 = "2024-01-24";
+  const date2 = "2024-01-24";
+
+  const daysExpected = 0;
+
+  expect(daysBetweenDates(date1, date2)).toEqual(daysExpected);
+});
